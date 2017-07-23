b893003e0b4b92c562e7a8ca4eb71a03c4ff5fad
Mrhamon11 <aviamon11@gmail.com>
2017-07-23 18:30:04 -0400

    "new class"

---
+++ projects/Shout Clock/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by aviam on 7/23/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ projects/Shout Clock/src/TimeFileLoader.java
@@ -0,0 +1,7 @@
+/**
+ * Created by aviam on 7/23/2017.
+ */
+public class TimeFileLoader {
+
+}
+
